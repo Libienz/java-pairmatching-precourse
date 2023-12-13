32b687bb4bc8262cab95750ae6ace2fc87b22400
libienz <Leekh10195@gmail.com>
2023-12-13 17:34:52 +0900

    feat: 올바른 입력 까지 반복 읽기 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import pairmatching.domain.FunctionMode;
 import pairmatching.domain.Procedure;
 import pairmatching.domain.RematchConfirm;
+import pairmatching.util.Repeater;
 
 public class InputView {
     private final InputMapper inputMapper;
@@ -18,18 +19,18 @@ public class InputView {
                 + "2. 페어 조회\n"
                 + "3. 페어 초기화\n"
                 + "Q. 종료");
-        return inputMapper.mapToFunctionMode(Console.readLine());
+        return Repeater.doRepeat(() -> inputMapper.mapToFunctionMode(Console.readLine()));
     }
 
     public RematchConfirm readRematch() {
         System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n"
                 + "네 | 아니오");
-        return inputMapper.mapToRematch(Console.readLine());
+        return Repeater.doRepeat(() -> inputMapper.mapToRematch(Console.readLine()));
     }
 
     public Procedure readProcedure() {
         System.out.println("과정, 레벨, 미션을 선택하세요.\n"
                 + "ex) 백엔드, 레벨1, 자동차경주");
-        return inputMapper.mapToProcedure(Console.readLine());
+        return Repeater.doRepeat(() -> inputMapper.mapToProcedure(Console.readLine()));
     }
 }
