03588c504d1ad20a56f35799c42056da31f3f981
libienz <Leekh10195@gmail.com>
2023-12-13 12:24:22 +0900

    feat: 코스와 미션을 속성으로 가지는 절차 Procedure 클래스 구현

---
+++ src/main/java/pairmatching/domain/Procedure.java
@@ -0,0 +1,11 @@
+package pairmatching.domain;
+
+public class Procedure {
+    private final Course course;
+    private final Mission mission;
+
+    public Procedure(Course course, Mission mission) {
+        this.course = course;
+        this.mission = mission;
+    }
+}
