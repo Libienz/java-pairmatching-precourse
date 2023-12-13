d3abc67350c839d307221aafe157f6a94c46e1ed
libienz <Leekh10195@gmail.com>
2023-12-13 11:42:07 +0900

    feat: Mission enum 작성

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,22 @@
+package pairmatching.domain;
+
+import static pairmatching.domain.Level.*;
+
+public enum Mission {
+    CAR_RACE("자동차경주", LEVEL1),
+    LOTTO("로또", LEVEL1),
+    BASEBALL("숫자야구", LEVEL1),
+    SHOP("장바구니", LEVEL2),
+    PAYMENT("결제", LEVEL2),
+    SUBWAY("지하철노선도", LEVEL2),
+    ENHANCEMENT("성능개선", LEVEL4),
+    PUBLISH("배포", LEVEL4);
+
+    private String name;
+    private Level level;
+
+    Mission(String name, Level level) {
+        this.name = name;
+        this.level = level;
+    }
+}
