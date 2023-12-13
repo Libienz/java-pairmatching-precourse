edef8ed1ba042b13b066efff787864b7f0690a9a
libienz <Leekh10195@gmail.com>
2023-12-13 12:33:41 +0900

    feat: 이름으로 Course 찾는 기능 구현

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,15 +1,24 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
 
+    private static final String NO_SUCH_COURSE_MESSAGE = "[ERROR] 해당하는 코스를 찾을 수 없습니다";
     private String name;
 
     Course(String name) {
         this.name = name;
     }
 
+    public static Course from(String name) {
+        return Arrays.stream(Course.values())
+                .filter(name::equals)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NO_SUCH_COURSE_MESSAGE));
+    }
     public String getName() {
         return name;
     }
