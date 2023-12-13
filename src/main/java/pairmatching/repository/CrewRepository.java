b435b03e416d1222cbeeb956c6be6c7439ce2158
libienz <Leekh10195@gmail.com>
2023-12-13 12:02:27 +0900

    feat: 레포지토리 생성 및 파일로 부터 데이터 load하는 기능 구현

---
+++ src/main/java/pairmatching/repository/CrewRepository.java
@@ -0,0 +1,40 @@
+package pairmatching.repository;
+
+import static pairmatching.domain.Course.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import pairmatching.domain.Course;
+import pairmatching.domain.Crew;
+
+public class CrewRepository {
+    private static final String BACKEND_CREW_FILE_PATH = "src/main/resources/backend-crew.md";
+    private static final String FRONTEND_CREW_FILE_PATH = "src/main/resources/frontend-crew.md";
+
+    private final List<Crew> crews;
+
+    public CrewRepository() throws IOException {
+        crews = new ArrayList<>();
+        init();
+    }
+
+    public void init() throws IOException {
+        FileReader fileReader = new FileReader(BACKEND_CREW_FILE_PATH);
+        BufferedReader br = new BufferedReader(fileReader);
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            crews.add(new Crew(BACKEND, line));
+        }
+
+        fileReader = new FileReader(FRONTEND_CREW_FILE_PATH);
+        br = new BufferedReader(fileReader);
+        line = "";
+        while ((line = br.readLine()) != null) {
+            crews.add(new Crew(FRONTEND, line));
+        }
+    }
+}
