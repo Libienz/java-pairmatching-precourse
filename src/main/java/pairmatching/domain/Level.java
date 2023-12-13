84eb1803981e456d1215ecc9d9662636713d2c61
libienz <Leekh10195@gmail.com>
2023-12-13 12:35:02 +0900

    feat: 이름으로 해당 하는 레벨 찾는 기능 구현

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,5 +1,7 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Level {
     LEVEL1("레벨1"),
     LEVEL2("레벨2"),
@@ -7,12 +9,19 @@ public enum Level {
     LEVEL4("레벨4"),
     LEVEL5("레벨5");
 
+    private static final String NO_SUCH_LEVEL_MESSAGE = "[ERROR] 해당하는 레벨을 찾을 수 없습니다";
     private String name;
 
     Level(String name) {
         this.name = name;
     }
 
+    public static Level from(String name) {
+        return Arrays.stream(Level.values())
+                .filter(name::equals)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NO_SUCH_LEVEL_MESSAGE));
+    }
     public String getName() {
         return name;
     }
