0e490e14bafbcef6a825b10fba2b2a94cd9f2307
libienz <Leekh10195@gmail.com>
2023-12-13 14:42:19 +0900

    feat: 페어 매칭 결과 repository 구현

---
+++ src/main/java/pairmatching/repository/PairMatchingRepository.java
@@ -0,0 +1,14 @@
+package pairmatching.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import pairmatching.domain.Pairs;
+import pairmatching.domain.Procedure;
+
+public class PairMatchingRepository {
+    public Map<Procedure, Pairs> map = new HashMap<>();
+
+    public Pairs findByProcedure(Procedure procedure) {
+        return map.getOrDefault(procedure, null);
+    }
+}
