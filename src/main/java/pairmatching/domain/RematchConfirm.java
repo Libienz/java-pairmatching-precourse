9e447a4447950c8e4694f5379811530b5810d032
libienz <Leekh10195@gmail.com>
2023-12-13 17:18:57 +0900

    feat: 매칭 이력이 있을 때 새로 매칭하는지 컨펌하는 도메인 클래스 작성

---
+++ src/main/java/pairmatching/domain/RematchConfirm.java
@@ -0,0 +1,22 @@
+package pairmatching.domain;
+
+import java.util.Arrays;
+
+public enum RematchConfirm {
+    YES("네"),
+    NO("아니오");
+
+    private static final String NO_SUCH_CONFIRM_MESSAGE = "[ERROR] 해당하는 내용이 없습니다";
+    private String code;
+
+    RematchConfirm(String code) {
+        this.code = code;
+    }
+
+    public RematchConfirm from(String code) {
+        return Arrays.stream(RematchConfirm.values())
+                .filter(rematchConfirm -> rematchConfirm.code.equals(code))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException());
+    }
+}
