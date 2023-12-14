3e4406a9fbee460890c92f7b11cc685237d47e54
libienz <Leekh10195@gmail.com>
2023-12-14 15:09:10 +0900

    fix: Pairs 일급 컬렉션 속성 오류 수정

--- src/main/java/pairmatching/domain/Pairs.java
+++ src/main/java/pairmatching/domain/Pairs.java
@@ -3,13 +3,13 @@ package pairmatching.domain;
 import java.util.List;
 
 public class Pairs {
-    private final List<Pairs> pairs;
+    private final List<Pair> pairs;
 
-    public Pairs(List<Pairs> pairs) {
+    public Pairs(List<Pair> pairs) {
         this.pairs = pairs;
     }
 
-    public List<Pairs> getPairs() {
+    public List<Pair> getPairs() {
         return pairs;
     }
 }
