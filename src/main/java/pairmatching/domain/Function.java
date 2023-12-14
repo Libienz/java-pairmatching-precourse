3e8dd7c0daf9ab5610bcfe42c84f17dc39726266
libienz <Leekh10195@gmail.com>
2023-12-14 14:01:59 +0900

    feat: 코드로 기능 찾는 기능 구현

--- src/main/java/pairmatching/domain/Function.java
+++ src/main/java/pairmatching/domain/Function.java
@@ -1,17 +1,27 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Function {
     PAIR_MATCH("1", "페어 매칭"),
     PAIR_READ("2", "페어 조회"),
     PAIR_RESET("3", "페어 초기화"),
     QUIT("Q", "종료");
 
+    private static final String NO_SUCH_FUNCTION_MESSAGE = "[ERROR] 해당하는 기능을 찾을 수 없습니다";
     private String code;
     private String name;
 
     Function(String code, String name) {
         this.code = code;
         this.name = name;
+    }
+
+    public static Function from(String code) {
+        return Arrays.stream(Function.values())
+                .filter(function -> function.code.equals(code))
+                .findFirst()
+                .orElseThrow((() -> new IllegalArgumentException(NO_SUCH_FUNCTION_MESSAGE)));
     }
 
     public String getCode() {
