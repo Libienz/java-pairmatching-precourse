07e8eccf4b86ad01ab742fa382fb81505a2ec8e1
libienz <Leekh10195@gmail.com>
2023-12-14 14:41:08 +0900

    feat: InputView 구현

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,31 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import pairmatching.domain.Confirm;
+import pairmatching.domain.Function;
+import pairmatching.domain.PairMissionCourse;
+
+public class InputView {
+    private final InputMapper inputMapper;
+    private final MessageResolver messageResolver;
+
+    public InputView(InputMapper inputMapper, MessageResolver messageResolver) {
+        this.inputMapper = inputMapper;
+        this.messageResolver = messageResolver;
+    }
+
+    public Function readFunction() {
+        System.out.println(messageResolver.resolveFunctionsMessage());
+        return inputMapper.mapToFunction(Console.readLine());
+    }
+
+    public PairMissionCourse readPairMissionCourse() {
+        System.out.println(messageResolver.resolveCourseAndMissionMessage());
+        return inputMapper.mapToPairsCourse(Console.readLine());
+    }
+
+    public Confirm readConfirm() {
+        System.out.println(messageResolver.resolveConfirmMessage());
+        return inputMapper.mapToConfirm(Console.readLine());
+    }
+}
