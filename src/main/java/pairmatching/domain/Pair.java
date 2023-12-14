d32235a8807c49c84d4e02f2240d23a07a3bef84
libienz <Leekh10195@gmail.com>
2023-12-14 15:09:35 +0900

    refactor: Pair 생성 로직 정적 팩토리 메서드로 수정

--- src/main/java/pairmatching/domain/Pair.java
+++ src/main/java/pairmatching/domain/Pair.java
@@ -1,10 +1,17 @@
 package pairmatching.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Pair {
-    private final List<Crew> pair = new ArrayList<>();
+    private final List<Crew> pair;
+
+    private Pair(List<Crew> pair) {
+        this.pair = pair;
+    }
+
+    public static Pair of(Crew crew1, Crew crew2) {
+        return new Pair(List.of(crew1, crew2));
+    }
 
     public List<Crew> getPair() {
         return pair;
