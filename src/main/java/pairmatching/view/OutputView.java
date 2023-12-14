2dfed9f6606a7eb83072eeae7c917a3f5be9ff50
libienz <Leekh10195@gmail.com>
2023-12-14 16:14:32 +0900

    feat: OutputView 구현

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,24 @@
+package pairmatching.view;
+
+import pairmatching.dto.MatchResultDto;
+import pairmatching.dto.PairReadResponseDto;
+
+public class OutputView {
+    private final MessageResolver messageResolver;
+
+    public OutputView(MessageResolver messageResolver) {
+        this.messageResolver = messageResolver;
+    }
+
+    public void printPairMatchResult(MatchResultDto matchResultDto) {
+        System.out.println(messageResolver.resolvePairMatchResultMessage(matchResultDto));
+    }
+
+    public void printPairRead(PairReadResponseDto pairReadResponseDto) {
+        System.out.println(messageResolver.resolvePairReadMessage(pairReadResponseDto));
+    }
+
+    public void printPairReset() {
+        System.out.println(messageResolver.resolvePairResetMessage());
+    }
+}
