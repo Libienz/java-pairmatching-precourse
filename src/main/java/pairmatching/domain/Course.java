96be102bf6d785a5caead1678704198618a1eded
libienz <Leekh10195@gmail.com>
2023-12-14 14:12:31 +0900

    feat: Course enum에 from 메서드 추가

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,12 +1,22 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
 
+    private static final String NO_SUCH_COURSE_MESSAGE = "[ERROR] 해당하는 코스를 찾을 수 없습니다";
     private String name;
 
     Course(String name) {
         this.name = name;
+    }
+
+    public static Course from(String name) {
+        return Arrays.stream(Course.values())
+                .filter(course -> course.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(NO_SUCH_COURSE_MESSAGE));
     }
 }
