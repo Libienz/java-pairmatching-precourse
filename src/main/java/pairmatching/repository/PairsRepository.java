dd68d5b5dc9a466545fbcad85877cf67271fab68
libienz <Leekh10195@gmail.com>
2023-12-14 15:29:45 +0900

    feat: 레벨별로 성사되었던 페어를 조회하는 기능 추가

--- src/main/java/pairmatching/repository/PairsRepository.java
+++ src/main/java/pairmatching/repository/PairsRepository.java
@@ -1,8 +1,12 @@
 package pairmatching.repository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import pairmatching.domain.Level;
+import pairmatching.domain.Pair;
 import pairmatching.domain.PairMissionCourse;
 import pairmatching.domain.Pairs;
 
@@ -15,5 +19,14 @@ public class PairsRepository {
 
     public Optional<Pairs> findPairsByCourse(PairMissionCourse course) {
         return Optional.ofNullable(matchedPairs.get(course));
+    }
+
+    public List<Pair> findPairByLevel(Level level) {
+        return matchedPairs.keySet().stream()
+                .filter(pairMissionCourse -> pairMissionCourse.getLevel().equals(level))
+                .map(matchedPairs::get)
+                .map(Pairs::getPairs)
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
     }
 }
