69ac72d30aa48444caed0853d0f6dd498725c930
libienz <Leekh10195@gmail.com>
2023-12-14 13:15:55 +0900

    feat: Mission enum 속성 정의 및 구현

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,32 @@
+package pairmatching.domain;
+
+import static pairmatching.domain.Level.LEVEL1;
+import static pairmatching.domain.Level.LEVEL2;
+import static pairmatching.domain.Level.LEVEL4;
+
+public enum Mission {
+    CAR_RACE("자동차경주", LEVEL1),
+    LOTTO("로또", LEVEL1),
+    NUMBER_BASEBALL("숫자야구", LEVEL1),
+    SHOP_CART("장바구니", LEVEL2),
+    PAYMENT("결제", LEVEL2),
+    SUBWAY("지하철노선도", LEVEL2),
+    PERFORMANCE_ENHANCEMENT("성능개선", LEVEL4),
+    PUBLISH("배포", LEVEL4);
+
+    private String name;
+    private Level level;
+
+    Mission(String name, Level level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+}
